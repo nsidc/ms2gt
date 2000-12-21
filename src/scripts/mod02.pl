4095c6c21c2bc81a2819216f61e0cf33e5c0ab97
Terry Haran <tharan@nsidc.org>
2000-12-21 23:37:25 +0000

    changed name to mod02.pl added multiple channels

--- src/scripts/mod02.pl
+++ src/scripts/mod02.pl
@@ -8,9 +8,25 @@ require("$source_navdir/error_mail.pl");
 require("$source_navdir/date.pl");
 
 my $Usage = "\n
-USAGE: modis.pl dirinout tag listfile gpdfile
-                [chan [swath_scan_first swath_scans]]
-defaults:         1            0             0\n\n";
+USAGE: mod02.pl dirinout tag listfile gpdfile
+                [chanlist [swath_scan_first [swath_scans]]]
+       defaults:    1            0               0
+
+  dirinout: directory containing the input and output files.
+  tag: string used as a prefix to output files.
+  listfile: text file containing a list of MOD02 files to be gridded.
+  gpdfile: .gpd file that defines desired output grid.
+  chanlist: string specifying channel numbers to be gridded. The default
+            is 1, i.e. grid channel 1 only.
+  swath_scan_first: the number of the first swath scan to process. The default
+                    is 0, i.e. the first scan in the swath.
+  swath_scans: the number of scans to process:
+                  1km (MOD021KM) files contain 10 rows per scan.
+                 500m (MOD02HKM) files contain 20 rows per scan.
+                 250m (MOD02QKM) files contain 40 rows per scan.
+               A typical 5 minute MOD02 granule contains 203 scans.
+               The default is 0, i.e. process all scans in the swath
+               following swath_scan_first.\n\n";
 
 #The following symbols are defined in pfsetup.pl and were used only once in
 #this module. They appear here to suppress warning messages.
@@ -19,7 +35,7 @@ my $junk = $script;
 
 # define a global used by do_or_die and invoke_or_die
 
-$script = "MODIS";
+$script = "MOD02";
 
 # Set command line defaults
 
@@ -27,7 +43,7 @@ my $dirinout;
 my $tag;
 my $listfile;
 my $gpdfile;
-my $chan = 1;
+my $chanlist = "1";
 my $swath_scan_first = 0;
 my $swath_scans = 0;
 
@@ -41,10 +57,12 @@ if (@ARGV <= 7) {
     $listfile = $ARGV[2];
     $gpdfile = $ARGV[3];
     if (@ARGV >= 5) {
-	$chan = sprintf("%02d", $ARGV[4]);
-	if (@ARGV >= 7) {
+	$chanlist = $ARGV[4];
+	if (@ARGV >= 6) {
 	    $swath_scan_first = $ARGV[5];
-	    $swath_scans = $ARGV[6];
+	    if (@ARGV >= 7) {
+		$swath_scans = $ARGV[6];
+	    }
 	}
     }
 } else {
@@ -58,7 +76,7 @@ print_stderr("\n".
 	     "> tag              = $tag\n".
 	     "> listfile         = $listfile\n".
 	     "> gpdfile          = $gpdfile\n".
-	     "> chan             = $chan\n".
+	     "> chanlist         = $chanlist\n".
 	     "> swath_scan_first = $swath_scan_first\n".
 	     "> swath_scans      = $swath_scans\n\n");
 
@@ -76,42 +94,55 @@ close(LISTFILE);
 my $hdf;
 my $swath_cols = 0;
 my $swath_rows = 0;
-my $chan_cat = "cat ";
 my $lat_cat = "cat ";
 my $lon_cat = "cat ";
+my $chan_count = length($chanlist);
+my @chan_cat;
+my @chans;
+my $i;
+for ($i = 0; $i < $chan_count; $i++) {
+    $chan_cat[$i] = "cat ";
+    $chans[$i] = sprintf("%02d", substr($chanlist, $i, 1));
+}
 my $swath_rows_per_scan;
 foreach $hdf (@list) {
     chomp $hdf;
+    my ($filestem) = ($hdf =~ /(.*)\.hdf/);
+    for ($i = 0; $i < $chan_count; $i++) {
+	my $chan = $chans[$i];
+	my $filestem_chan = $filestem . "_ch$chan\_";
+	do_or_die("idl_sh.pl extract_chan \"'$hdf'\" $chan");
+	my @chan_glob = glob("$filestem_chan*");
+	my $chan_file = $chan_glob[0];
+	my ($this_swath_cols, $this_swath_rows) =
+	    ($chan_file =~ /$filestem_chan(.....)_(.....)/);
+	print "$chan_file contains $this_swath_cols cols and " .
+	    "$this_swath_rows rows\n";
+	if ($swath_cols == 0) {
+	    $swath_cols = $this_swath_cols;
+	}
+	if ($this_swath_cols != $swath_cols) {
+	    diemail("$script: FATAL: " .
+		    "inconsistent number of columns in $chan_file");
+	}
+	$chan_cat[$i] .= "$chan_file ";
+	if ($i == 0) {
+	    $swath_rows += $this_swath_rows;
+	}
+    }
+
     my ($resolution) = ($hdf =~ /MOD02(.)/);
     $swath_rows_per_scan = 10;
+    my $interp_factor = 1;
+    my $hdf_latlon = $hdf;
     if ($resolution eq "1") {
-	do_or_die("idl_sh.pl extract_chan \"'$hdf'\" $chan");
-	my $hdf_500 = $hdf;
-	$hdf_500 =~ s/1KM/HKM/;
-	do_or_die("idl_sh.pl extract_latlon \"'$hdf_500'\" 1");
+	$hdf_latlon =~ s/1KM/HKM/;
     } else {
-	my $interp_factor = ($resolution eq "H") ? 2 : 4;
+	$interp_factor = ($resolution eq "H") ? 2 : 4;
 	$swath_rows_per_scan *= $interp_factor;
-	do_or_die("idl_sh.pl extract_latlon \"'$hdf'\" " .
-		  "$interp_factor channel=$chan");
     }
-    my ($filestem) = ($hdf =~ /(.*)\.hdf/);
-    my $filestem_chan = $filestem . "_ch$chan\_";
-    my @chan_glob = glob("$filestem_chan*");
-    my $chan_file = $chan_glob[0];
-    my ($this_swath_cols, $this_swath_rows) =
-	($chan_file =~ /$filestem_chan(.....)_(.....)/);
-    print "$chan_file contains $this_swath_cols cols and " .
-	  "$this_swath_rows rows\n";
-    if ($swath_cols == 0) {
-	$swath_cols = $this_swath_cols;
-    }
-    if ($this_swath_cols != $swath_cols) {
-	diemail("$script: FATAL: " .
-		"inconsistent number of columns in $chan_file");
-    }
-    $swath_rows += $this_swath_rows;
-    $chan_cat .= "$chan_file ";
+    do_or_die("idl_sh.pl extract_latlon \"'$hdf_latlon'\" " .
+	      "$interp_factor");
 
     my $filestem_lat = $filestem . "_latf_";
     my @lat_glob = glob("$filestem_lat*");
@@ -120,8 +151,7 @@ foreach $hdf (@list) {
 	($lat_file =~ /$filestem_lat(.....)_(.....)/);
     print "$lat_file contains $this_lat_cols cols and " .
 	  "$this_lat_rows rows\n";
-    if ($this_lat_cols != $this_swath_cols ||
-	$this_lat_rows != $this_swath_rows) {
+    if ($this_lat_cols != $swath_cols) {
 	diemail("$script: FATAL: " .
 		"inconsistent size for $lat_file");
     }
@@ -134,8 +164,7 @@ foreach $hdf (@list) {
 	($lon_file =~ /$filestem_lon(.....)_(.....)/);
     print "$lon_file contains $this_lon_cols cols and " .
 	  "$this_lon_rows rows\n";
-    if ($this_lon_cols != $this_swath_cols ||
-	$this_lon_rows != $this_swath_rows) {
+    if ($this_lon_cols != $swath_cols) {
 	diemail("$script: FATAL: " .
 		"inconsistent size for $lon_file");
     }
@@ -143,23 +172,28 @@ foreach $hdf (@list) {
 }
 $swath_rows = sprintf("%05d", $swath_rows);
 
-my $chan_rm = $chan_cat;
+my @chan_files;
+for ($i = 0; $i < $chan_count; $i++) {
+    my $chan = $chans[$i];
+    my $chan_rm = $chan_cat[$i];
+    $chan_rm =~ s/cat/rm -f/;
+    $chan_files[$i] = "$tag\_ch$chan\_$swath_cols\_$swath_rows.img";
+    do_or_die("$chan_cat[$i] >$chan_files[$i]");
+    do_or_die("$chan_rm");
+}
+
 my $lat_rm = $lat_cat;
 my $lon_rm = $lon_cat;
 
-$chan_rm =~ s/cat/rm -f/;
 $lat_rm  =~ s/cat/rm -f/;
 $lon_rm  =~ s/cat/rm -f/;
 
-my $chan_file = "$tag\_ch$chan\_$swath_cols\_$swath_rows.img";
 my $lat_file  = "$tag\_latf_$swath_cols\_$swath_rows.img";
 my $lon_file  = "$tag\_lonf_$swath_cols\_$swath_rows.img";
 
-do_or_die("$chan_cat >$chan_file");
 do_or_die("$lat_cat  >$lat_file");
 do_or_die("$lon_cat  >$lon_file");
 
-do_or_die("$chan_rm");
 do_or_die("$lat_rm");
 do_or_die("$lon_rm");
 
@@ -174,15 +208,29 @@ my $line = <GPDFILE>;
 $line = <GPDFILE>;
 close(GPDFILE);
 my ($grid_cols, $grid_rows) = ($line =~ /(\S+)\s+(\S+)/);
-my $grid_file = "$tag\_ch$chan\_grid\_$grid_cols\_$grid_rows.img";
 if ($swath_scans eq "0") {
-    $swath_scans = $swath_rows / $swath_rows_per_scan;
+    $swath_scans = $swath_rows / $swath_rows_per_scan - $swath_scan_first;
 }
+
+my $chan_file_param = "[";
+my $grid_file_param = "[";
+for ($i = 0; $i < $chan_count; $i++) {
+    if ($i > 0) {
+	$chan_file_param .= ",";
+	$grid_file_param .= ",";
+    }
+    my $chan_file = $chan_files[$i];
+    my $chan = $chans[$i];
+    my $grid_file = "$tag\_ch$chan\_grid\_$grid_cols\_$grid_rows.img";
+    $chan_file_param .= "\"'$chan_file'\"";
+    $grid_file_param .= "\"'$grid_file'\"";
+}
+$chan_file_param .= "]";
+$grid_file_param .= "]";
 
 do_or_die("idl_sh.pl fornav " .
 	  "$swath_cols $swath_scans $swath_rows_per_scan " .
-	  "\"'$col_file'\" \"'$row_file'\" \"'$chan_file'\" " .
-	  "$grid_cols $grid_rows " .
-	  "\"'$grid_file'\" " .
+	  "\"'$col_file'\" \"'$row_file'\" $chan_file_param " .
+	  "$grid_cols $grid_rows $grid_file_param " .
 	  "weight_sum_min=0.001 " .
 	  "swath_scan_first=$swath_scan_first");
